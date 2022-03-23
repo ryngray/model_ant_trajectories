a0ca7b07b3070e41beea897a67666d41fe408d6f
Ryn Gray <ryngray@email.arizona.edu>
2022-03-23 11:12:22 -0700

    Updated histogram

--- comparisons/histogram_comparisons.py
+++ comparisons/histogram_comparisons.py
@@ -142,7 +142,7 @@ def speed_calcs(dat_given, dat_avg):
 
 	print(np.shape(data_given))
 	for i in tqdm(range(num_generate)):
-	    rs_tmp = (get_speed(data_given[data_given[:,0] == i],0))
+	    rs_tmp = (get_speed(data_given[data_given[:,2] == i],0))
 	    gan_speed.append(np.mean(rs_tmp))
 	    gan_max.append(np.max(rs_tmp))
 	    gan_mode.append(stats.mode(rs_tmp)[0])
@@ -157,6 +157,12 @@ def speed_calcs(dat_given, dat_avg):
 	plt.title("Histogram of mode of speed")
 	plt.xlabel("m/s")
 	plt.legend()
+	md = np.mean(dat_avg['mode_speed'])
+	vd = np.std(dat_avg['mode_speed'])
+	mg = np.mean(gan_mode)
+	vg = np.std(gan_mode)
+	str_box = "Data Mean: " + str(round(md,2)) + "\nData STD: " + str(round(vd,2)) + "\nGenerated Mean: " +str(round(mg,2)) + "\nGenerated STD: " + str(round(vg,2))
+	plt.annotate(str_box, xy=(0.05, 0.8), xycoords='axes fraction')
 	plt.savefig("mode_speed.png")
 
 	print("KS Mode Speed", stats.ks_2samp(dat_avg['mode_speed'], gan_mode))
@@ -168,7 +174,14 @@ def speed_calcs(dat_given, dat_avg):
 	plt.xlabel("m/s")
 	plt.savefig("max_speed.png")
 	plt.legend()
-	plt.show()
+	md = np.mean(dat_avg['max_speed'])
+	vd = np.std(dat_avg['max_speed'])
+	mg = np.mean(gan_mode)
+	vg = np.std(gan_mode)
+	str_box = "Data Mean: " + str(md) + "\nData STD: " + str(vd) + "\nGenerated Mean: " +str(mg) + "\nGenerated STD: " + str(vg)
+	# plt.text(0.05, 0.95, str_box, verticalalignment = "top")
+	plt.annotate(str_box, xy=(0.05, 0.8), xycoords='axes fraction')
+	plt.savefig("max_speed")
 
 	print("KS Max Speed", stats.ks_2samp(dat_avg['max_speed'],gan_max))
 
@@ -181,7 +194,7 @@ def acceleration_calcs(dat_given, dat_avg):
 	gan_acc_mode = []
 	num_generate = min([len(data_given), len(dat_avg)])
 	for i in tqdm(range(num_generate)):
-	    rs_tmp = get_acceleration(data_given[data_given[:,0] == i],0)
+	    rs_tmp = get_acceleration(data_given[data_given[:,2] == i],0)
 	    gan_acc.append(np.mean(rs_tmp))
 	    gan_acc_max.append(np.max(rs_tmp))
 	    gan_acc_mode.append(stats.mode(rs_tmp)[0])
@@ -191,8 +204,14 @@ def acceleration_calcs(dat_given, dat_avg):
 	plt.figure(2)
 	plt.hist(dat_avg['mode_acceleration'],bins = 50, label="Real Data",alpha=0.5)
 	plt.hist(gan_acc_mode, bins= 50, label="Generated Data",alpha=0.5)
-	plt.title("Comparison of Average Acceleration")
+	plt.title("Comparison of Mode Acceleration")
 	plt.legend()
+	md = np.mean(dat_avg['mode_acceleration'])
+	vd = np.std(dat_avg['mode_acceleration'])
+	mg = np.mean(gan_acc_mode)
+	vg = np.std(gan_acc_mode)
+	str_box = "Data Mean: " + str(round(md,2)) + "\nData STD: " + str(round(vd,2)) + "\nGenerated Mean: " +str(round(mg,2)) + "\nGenerated STD: " + str(round(vg,2))
+	plt.annotate(str_box, xy=(0.05, 0.8), xycoords='axes fraction')
 	plt.savefig("acceleration_gan.png")
 
 	print("KS Mode Acceleration:", stats.ks_2samp(dat_avg["mode_acceleration"], gan_acc_mode))
@@ -202,6 +221,12 @@ def acceleration_calcs(dat_given, dat_avg):
 	plt.hist(gan_acc_max, bins= 50, label="Generated Data",alpha=0.5)
 	plt.title("Comparison of Max Acceleration")
 	plt.legend()
+	md = np.mean(dat_avg['max_acceleration'])
+	vd = np.std(dat_avg['max_acceleration'])
+	mg = np.mean(gan_acc_max)
+	vg = np.std(gan_acc_max)
+	str_box = "Data Mean: " + str(round(md,2)) + "\nData STD: " + str(round(vd,2)) + "\nGenerated Mean: " +str(round(mg,2)) + "\nGenerated STD: " + str(round(vg,2))
+	plt.annotate(str_box, xy=(0.05, 0.8), xycoords='axes fraction')	
 	plt.savefig("acceleration_max_gan.png")
 
 	print("KS Max Acceleration:", stats.ks_2samp(dat_avg['max_acceleration'], gan_acc_max))
@@ -214,7 +239,7 @@ def angle_calcs(dat_given, dat_avg):
 	gan_ang_mode = []
 	num_generate = min([len(data_given), len(dat_avg)])
 	for i in range(num_generate):
-	    rs_tmp = get_angle(data_given[data_given[:,0] == i])
+	    rs_tmp = get_angle(data_given[data_given[:,2] == i])
 	    gan_ang.append(math.radians(np.mean(rs_tmp)))
 	    gan_ang_max.append(math.radians(np.max(rs_tmp)))
 	    gan_ang_mode.append(math.radians(stats.mode(rs_tmp)[0]))
@@ -222,20 +247,32 @@ def angle_calcs(dat_given, dat_avg):
 	gan_ang_mode = np.reshape(gan_ang_mode, (num_generate))
 	# rw_speed  = [item for sublist in s for item in sublist]
 
-
+	plt.figure(4)
 	plt.hist(dat_avg['max_angle'],bins = 50, label="Real Data",alpha=0.5)
 	plt.hist(gan_ang_max, bins= 50, label="GAN Generated Data",alpha=0.5)
 	plt.title("Comparison of Max Angle")
 	plt.legend()
+	md = np.mean(dat_avg['max_angle'])
+	vd = np.std(dat_avg['max_angle'])
+	mg = np.mean(gan_ang_max)
+	vg = np.std(gan_ang_max)
+	str_box = "Data Mean: " + str(round(md,2)) + "\nData STD: " + str(round(vd,2)) + "\nGenerated Mean: " +str(round(mg,2)) + "\nGenerated STD: " + str(round(vg,2))
+	plt.annotate(str_box, xy=(0.05, 0.8), xycoords='axes fraction')
 	plt.savefig("angle_gan.png")
 	print("KS Angle Mode:", stats.ks_2samp(dat_avg['mode_angle'], gan_ang_max))
 
 
-
+	plt.figure(5)
 	plt.hist(dat_avg['mode_angle'],bins = 50, label="Real Data",alpha=0.5)
 	plt.hist(gan_ang_mode, bins= 50, label="GAN Generated Data",alpha=0.5)
 	plt.title("Comparison of Mode Angle (in degrees)")
 	plt.legend()
+	md = np.mean(dat_avg['mode_angle'])
+	vd = np.std(dat_avg['mode_angle'])
+	mg = np.mean(gan_ang_mode)
+	vg = np.std(gan_ang_mode)
+	str_box = "Data Mean: " + str(round(md,2)) + "\nData STD: " + str(round(vd,2)) + "\nGenerated Mean: " +str(round(mg,2)) + "\nGenerated STD: " + str(round(vg,2))
+	plt.annotate(str_box, xy=(0.05, 0.8), xycoords='axes fraction')
 	plt.savefig("angle_gan.png")
 
 	print("KS Angle Max:", stats.ks_2samp(dat_avg['max_angle'], gan_ang_max))
@@ -282,12 +319,22 @@ def get_ganpd(traj_sim, num_generate, padding=200):
 	return gan_pd	
 
 def straight_line_distance_calcs(tdf, dat_avg):
-	dsl_df = distance_straight_line(tdf)
+	try:
+		dsl_df = distance_straight_line(tdf)
+	except:
+		return 0
 
+	plt.figure(6)
 	plt.hist(dat_avg['dist_traveled'],bins=50, label="Real Data",alpha=0.5)
 	plt.hist(dsl_df['distance_straight_line'],bins=50, label="GAN Generated Data",alpha=0.5)
 	plt.title("Comparison of Straight Line Distance")
 	plt.legend()
+	md = np.mean(dat_avg['dist_traveled'])
+	vd = np.std(dat_avg['dist_traveled'])
+	mg = np.mean(dsl_df['distance_straight_line'],)
+	vg = np.std(dsl_df['distance_straight_line'],)
+	str_box = "Data Mean: " + str(round(md,2)) + "\nData STD: " + str(round(vd,2)) + "\nGenerated Mean: " +str(round(mg,2)) + "\nGenerated STD: " + str(round(vg,2))
+	plt.annotate(str_box, xy=(0.05, 0.8), xycoords='axes fraction')
 	plt.savefig("line_dist_gan.png")
 
 	print("KS Straight Line Distance:", stats.ks_2samp(dat_avg['dist_to_line'], dsl_df['distance_straight_line']))
@@ -296,11 +343,21 @@ def straight_line_distance_calcs(tdf, dat_avg):
 
 
 def max_dist_calcs(tdf, dat_avg):
-	md_df = maximum_distance(tdf)
+	try:
+		md_df = maximum_distance(tdf)
+	except:
+		return 0
+	plt.figure(7)
 	plt.hist(dat_avg['max_dist'],bins=50, label="Real Data",alpha=0.5)
 	plt.hist(md_df['maximum_distance'],bins=50, label="GAN Generated Data",alpha=0.5)
 	plt.title("Comparison of Maximum distance between any two points")
 	plt.legend()
+	md = np.mean(dat_avg['max_dist'])
+	vd = np.std(dat_avg['max_dist'])
+	mg = np.mean(md_df['maximum_distance'])
+	vg = np.std(md_df['maximum_distance'])
+	str_box = "Data Mean: " + str(round(md,2)) + "\nData STD: " + str(round(vd,2)) + "\nGenerated Mean: " +str(round(mg,2)) + "\nGenerated STD: " + str(round(vg,2))
+	plt.annotate(str_box, xy=(0.05, 0.8), xycoords='axes fraction')
 	plt.savefig("max_dist_gan.png")
 
 	print("KS Maximum distance between two points", stats.ks_2samp(dat_avg['max_dist'], md_df['maximum_distance']))
@@ -321,12 +378,19 @@ def distance_straight_line_calcs(tdf, dat_avg):
 	        d_tmp += norm(np.cross(p2-p1, p1-p3))/norm(p2-p1)
 	    d_sl.append(d_tmp)
 
-	    plt.hist(dat_avg['dist_to_line'],bins=50, label="Real Data",alpha=0.5)
+	plt.hist(dat_avg['dist_to_line'],bins=50, label="Real Data",alpha=0.5)
+	plt.figure(8)
 	plt.hist(d_sl, bins=10, label="GAN Generated Data",alpha=0.5)
 	plt.title("Comparison of Distance to a straight line")
 	# plt.xlim((0,1))
 	plt.ylim((0,400))
 	plt.legend()
+	md = np.mean(dat_avg['dist_to_line'])
+	vd = np.std(dat_avg['dist_to_line'])
+	mg = np.mean(d_sl)
+	vg = np.std(d_sl)
+	str_box = "Data Mean: " + str(round(md,2)) + "\nData STD: " + str(round(vd,2)) + "\nGenerated Mean: " +str(round(mg,2)) + "\nGenerated STD: " + str(round(vg,2))
+	plt.annotate(str_box, xy=(0.05, 0.8), xycoords='axes fraction')
 	plt.savefig("max_dist_gan.png")
 
 def direction_changes_calcs(gan_pd, dat_avg):
@@ -336,10 +400,17 @@ def direction_changes_calcs(gan_pd, dat_avg):
 	for i in tqdm(range(num_generate)):
 	    ang = get_angle(np.asarray(gan_pd[gan_pd['user']==i]))
 	    dc.append(get_change_dir(ang))
+	plt.figure(9)
 	plt.hist(dat_avg['direction_changes'],bins=50, label="Real Data",alpha=0.5)
 	plt.hist(dc,bins=50, label="GAN Generated Data",alpha=0.5)
 	plt.title("Comparison of Direction Changes")
 	plt.legend()
+	md = np.mean(dat_avg['direction_changes'])
+	vd = np.std(dat_avg['direction_changes'])
+	mg = np.mean(dc)
+	vg = np.std(dc)
+	str_box = "Data Mean: " + str(round(md,2)) + "\nData STD: " + str(round(vd,2)) + "\nGenerated Mean: " +str(round(mg,2)) + "\nGenerated STD: " + str(round(vg,2))
+	plt.annotate(str_box, xy=(0.05, 0.8), xycoords='axes fraction')
 	plt.savefig("dir_changes_gan.png")
 
 	print("KS Direction Changes:", stats.ks_2samp(dat_avg['direction_changes'], dc))
@@ -357,11 +428,17 @@ def triangle_area_calcs(gan_pd, dat_avg):
 	    c = newa[newa[:,2]==plotid]
 	    area = triangle_area(c,step_size=30)
 	    areas.append(abs(area))
-
+	plt.figure(10)
 	plt.hist(dat_avg['triangle_area'],bins=50, label="Real Data")
 	plt.hist(areas, bins=50, label="GAN Generated Data")
 	plt.title("Comparison of triangle area")
 	plt.legend()
+	md = np.mean(dat_avg['triangle_area'])
+	vd = np.std(dat_avg['triangle_area'])
+	mg = np.mean(areas)
+	vg = np.std(areas)
+	str_box = "Data Mean: " + str(round(md,2)) + "\nData STD: " + str(round(vd,2)) + "\nGenerated Mean: " +str(round(mg,2)) + "\nGenerated STD: " + str(round(vg,2))
+	plt.annotate(str_box, xy=(0.05, 0.8), xycoords='axes fraction')
 	# plt.xlim((0,10000000))
 	plt.savefig("max_from_start_gan.png")
 
@@ -382,10 +459,17 @@ def max_from_start_calcs(gan_pd, dat_avg):
 	    dists = np.sqrt(np.add(np.power(xs, 2), np.power(ys, 2)))
 	    m = max(dists)
 	    max_dists.append(m)
+	plt.figure(11)
 	plt.hist(dat_avg['max_dist_start'],bins=50, label="Real Data",alpha=0.5)
 	plt.hist(max_dists, bins=50, label="GAN Generated Data",alpha=0.5)
 	plt.title("Comparison of max distance from start")
 	plt.legend()
+	md = np.mean(dat_avg['max_dist_start'])
+	vd = np.std(dat_avg['max_dist_start'])
+	mg = np.mean(max_dists)
+	vg = np.std(max_dists)
+	str_box = "Data Mean: " + str(round(md,2)) + "\nData STD: " + str(round(vd,2)) + "\nGenerated Mean: " +str(round(mg,2)) + "\nGenerated STD: " + str(round(vg,2))
+	plt.annotate(str_box, xy=(0.05, 0.8), xycoords='axes fraction')
 	# plt.xlim((0,10))
 	plt.savefig("max_from_start_gan.png")
 
@@ -402,12 +486,18 @@ def crossings_calcs(gan_pd, dat_avg):
 	for plotid in tqdm(range(num_generate)):
 	    c = newa[newa[:,2]==plotid]
 	    crossings_gan.append(get_crossings(c))
-
+	plt.figure(12)
 	plt.hist(dat_avg['num_crossings'],bins=50, label="Real Data",alpha=0.5)
 	plt.hist(crossings_gan, bins=50, label="GAN Generated Data",alpha=0.5)
 	plt.title("Comparison of Crossings")
 	plt.legend()
 	# plt.xlim((0,10))
+	md = np.mean(dat_avg['num_crossings'])
+	vd = np.std(dat_avg['num_crossings'])
+	mg = np.mean(crossings_gan)
+	vg = np.std(crossings_gan)
+	str_box = "Data Mean: " + str(round(md,2)) + "\nData STD: " + str(round(vd,2)) + "\nGenerated Mean: " +str(round(mg,2)) + "\nGenerated STD: " + str(round(vg,2))
+	plt.annotate(str_box, xy=(0.05, 0.8), xycoords='axes fraction')
 	plt.savefig("crossings_gan.png")
 
 
@@ -428,7 +518,7 @@ if __name__ == "__main__":
 	filename_avg = sys.argv[2]
 
 	data_given = pd.read_csv(filename_data,header=None)
-	data_given = np.asarray(data_given)
+	data_given = np.asarray(data_given).astype(float)
 
 	dat_avg = pd.read_csv(filename_avg)
 
@@ -459,5 +549,6 @@ if __name__ == "__main__":
 
 
 
-
+'''[[KstestResult(statistic=0.6049012253063266, pvalue=0.0), KstestResult(statistic=0.9992498124531133, pvalue=0.0)], [KstestResult(statistic=0.9997499374843711, pvalue=0.0), KstestResult(statistic=0.9997499374843711, pvalue=0.0)], [KstestResult(statistic=0.34783695923980995, pvalue=5.53005669150203e-215), KstestResult(statistic=0.9414853713428357, pvalue=0.0)], [KstestResult(statistic=0.980557748812203, pvalue=0.0)], [KstestResult(statistic=0.9930921245936484, pvalue=0.0)], [KstestResult(statistic=0.9959989997499374, pvalue=0.0)], [KstestResult(statistic=0.9987496874218554, pvalue=0.0)], [KstestResult(statistic=0.9924981245311327, pvalue=0.0)], [KstestResult(statistic=0.5481370342585646, pvalue=0.0)]]
+'''
 
